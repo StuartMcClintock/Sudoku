fd8023178803e385c4e4317e9aa579504fc205a4
Stuart McClintock <Stuart.e.mcc@gmail.com>
2019-12-26 15:11:58 -0800

    Initial Commit

---
+++ Sudoku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sudoku
+//
+//  Created by Stuart McClintock on 12/26/19.
+//  Copyright © 2019 Stuart McClintock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
